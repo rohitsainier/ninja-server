f6a4f7539f6fb8774996b198dc6b0395a3a1eba7
Rohit Saini <sketchlearn7@gmail.com>
2020-04-13 13:29:57 +0530

    Promises file modified

--- promises.js
+++ promises.js
@@ -1,6 +1,14 @@
 const promise = new Promise((resolve,reject) =>{
-    //resolve('Success');
-    reject('Erron in Promise')
+    resolve('Success');
+    //reject('Erron in Promise')
+})
+.then(value =>{
+    console.log(value);
+    return 1
+})
+.then(value =>{
+    console.log(value);
+    return 2
 })
 .then(value =>{
     console.log(value);
